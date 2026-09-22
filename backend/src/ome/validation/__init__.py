d863ecf6ecfd28eccd5f4eb6dd93d0ffbeb42387
Allef Viana <allefribeirov1@gmail.com>
2026-09-22 19:36:11 -0300

    refactor: remove unversioned validation check injection

--- backend/src/ome/validation/__init__.py
+++ backend/src/ome/validation/__init__.py
@@ -1,11 +1,10 @@
 """Non-destructive telemetry validation."""
 
 from ome.validation.checks import check_metadata, check_time_structure, check_values
-from ome.validation.service import TelemetryValidator, ValidationCheck
+from ome.validation.service import TelemetryValidator
 
 __all__ = [
     "TelemetryValidator",
-    "ValidationCheck",
     "check_metadata",
     "check_time_structure",
     "check_values",
